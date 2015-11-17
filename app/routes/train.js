ea9b79f55bbf4ac3ee56e29a7b026b3c1c981245
mengleisun <sml0634@ufl.edu>
2015-11-17 18:35:41 -0500

    change some little issues

--- app/routes/train.js
+++ app/routes/train.js
@@ -1,10 +1,11 @@
 // train routers
 var express = require('express');
+var os = require('os');
 var TrainHandler = express.Router();
 TrainHandler.use(express.logger('dev'));
 TrainHandler.use(express.bodyParser());
 TrainHandler.use(app.router);
-
+var Train = require('../models/train').Train;
 // middleware to use for all requests
 //TrainHandler.use(function(req, res, next) {
 //    // do logging
@@ -24,36 +25,60 @@ TrainHandler.get('/', function(req, res) {
 // on routes that end in /bears
 // ----------------------------------------------------
 var openConnections = [];
-TrainHandler.route('/train')
+TrainHandler.route('/')
 
     .post(function(req, res) {
-
-        var python = require('child_process').spawn(
-            'python',
-            // second argument is array of parameters, e.g.:
-            ["DeepLearner.py"
-                , req.body.dataSet
-                , req.body.sequence
-                , req.body.percentage
-                , req.body.validation
-                , req.body.innerActivation
-                , req.body.activation
-                , req.body.dropOut
-                , req.body.lossFunction
-                , req.body.classMode
-                , req.body.finalActivationFunction]
+        var train = new Train();
+        if (req.body.model === "LSTM") {
+            train.engine = req.body.engine;
+            train.model = req.body.model;
+            train.dataSet = req.body.dataSet;
+            train.sequence = req.body.sequence;
+            train.percentage = req.body.percentage;
+            train.validation = req.body.validation;
+            train.innerActivation = req.body.innerActivation;
+            train.activation = req.body.activation;
+            train.dropOut = req.body.dropOut;
+            train.lossFunction = req.body.lossFunction;
+            train.classMode = req.body.classMode;
+            train.finalActivationFunction = req.body.finalActivationFunction;
+        }
+        if (req.body.model === "RNN") {
+            ///////////
+        }
+        if (req.body.model === "CNN") {
+            ///
+        }
+        if (req.body.engine === "keras") {
+            var python = require('child_process').spawn(
+                'python',
+                // second argument is array of parameters, e.g.:
+                ["DeepLearner.py"
+                    , req.body.model
+                    , req.body.dataSet
+                    , req.body.sequence
+                    , req.body.percentage
+                    , req.body.validation
+                    , req.body.innerActivation
+                    , req.body.activation
+                    , req.body.dropOut
+                    , req.body.lossFunction
+                    , req.body.classMode
+                    , req.body.finalActivationFunction]
             );
-        var output = "";
-        python.stdout.on('data', function(){ output += data });
-        python.on('close', function(code){
-            if (code !== 0) {  return res.send(500, code); }
-            return res.send(200, output)
-        });
+            var output = "";
+            python.stdout.on('data', function(){ output += data });
+            python.on('close', function(code){
+                if (code !== 0) {  return res.send(500, code); }
+                return res.send(200, output)
+            });
+        }
+
     })
 
     // get all the bears (accessed at GET http://localhost:8080/api/bears)
     .get(function(req, res) {
-        Map.find(function(err, result) {
+        Train.find(function(err, result) {
             if (err)
                 res.send(err);
 
