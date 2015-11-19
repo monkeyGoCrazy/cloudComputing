3e34193f1993f39a2e091cce346620ace3dc7e00
mengleisun <sml0634@ufl.edu>
2015-11-19 11:55:09 -0500

    finish the first version of the train route

--- app/routes/train.js
+++ app/routes/train.js
@@ -1,11 +1,22 @@
 // train routers
 var express = require('express');
+var server = require('http').Server(app);
+var app = express();
+var io = require('socket.io')(server);
+server.listen(8088,'127.0.0.1');
+var spawn = require('child_process').spawn;
 var os = require('os');
+var util = require('util');
+var http = require('http');
+var path = require('path');
 var TrainHandler = express.Router();
-TrainHandler.use(express.logger('dev'));
-TrainHandler.use(express.bodyParser());
-TrainHandler.use(app.router);
+var morgan     = require('morgan');
+var bodyParser = require('body-parser');
+TrainHandler.use(morgan('dev')); // log requests to the console
 var Train = require('../models/train').Train;
+var Statistics = require('../models/statistics2').Statistics;
+TrainHandler.use(bodyParser.urlencoded({ extended: true }));
+TrainHandler.use(bodyParser.json());
 // middleware to use for all requests
 //TrainHandler.use(function(req, res, next) {
 //    // do logging
@@ -18,30 +29,55 @@ TrainHandler.use(function(req, res, next) {
     next();
 });
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-TrainHandler.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to ddos world!' });
+TrainHandler.get('/', function(req, res,next) {
+    Train.find(function(err, result) {
+        if (err)
+            res.send(err);
+
+        res.json(result);
+    });
+    next();
 });
 
-// on routes that end in /bears
+// on routes that end in /new
 // ----------------------------------------------------
-var openConnections = [];
-TrainHandler.route('/')
+TrainHandler.route('/new')
 
     .post(function(req, res) {
+        var output = "";
+        var statistic = new Statistics();
+        statistic.save(function(err) {
+            if (err)
+                res.send(err);
+        });
+        var stat = spawn('python',
+            ["Statistic.py"
+                ,statistic._id
+                ,req.body.dataSet
+            ]);
+        stat.stderr.on('data',function(data) {
+            console.log('Failed to start child process.');
+            output += data + "\n";
+        });
+        stat.stdout.on('data', function(data) {
+            console.log(data);
+            output += data + "\n";
+        });
         var train = new Train();
+        train._id = statistic._id;
         if (req.body.model === "LSTM") {
             train.engine = req.body.engine;
             train.model = req.body.model;
             train.dataSet = req.body.dataSet;
-            train.sequence = req.body.sequence;
-            train.percentage = req.body.percentage;
-            train.validation = req.body.validation;
-            train.innerActivation = req.body.innerActivation;
-            train.activation = req.body.activation;
-            train.dropOut = req.body.dropOut;
-            train.lossFunction = req.body.lossFunction;
-            train.classMode = req.body.classMode;
-            train.finalActivationFunction = req.body.finalActivationFunction;
+            train.LSTMParameters.sequence = req.body.sequence;
+            train.LSTMParameters.percentage = req.body.percentage;
+            train.LSTMParameters.validation = req.body.validation;
+            train.LSTMParameters.innerActivation = req.body.innerActivation;
+            train.LSTMParameters.activation = req.body.activation;
+            train.LSTMParameters.dropOut = req.body.dropOut;
+            train.LSTMParameters.lossFunction = req.body.lossFunction;
+            train.LSTMParameters.classMode = req.body.classMode;
+            train.LSTMParameters.finalActivationFunction = req.body.finalActivationFunction;
         }
         if (req.body.model === "RNN") {
             ///////////
@@ -49,28 +85,51 @@ TrainHandler.route('/')
         if (req.body.model === "CNN") {
             ///
         }
+        train.save(function(err) {
+            if (err)
+                res.send(err);
+        });
+
         if (req.body.engine === "keras") {
-            var python = require('child_process').spawn(
-                'python',
-                // second argument is array of parameters, e.g.:
-                ["DeepLearner.py"
-                    , req.body.model
-                    , req.body.dataSet
-                    , req.body.sequence
-                    , req.body.percentage
-                    , req.body.validation
-                    , req.body.innerActivation
-                    , req.body.activation
-                    , req.body.dropOut
-                    , req.body.lossFunction
-                    , req.body.classMode
-                    , req.body.finalActivationFunction]
+            var child = spawn('python',
+                    ["DeepLearner.py"
+                        , req.body.model
+                        , req.body.dataSet
+                        , req.body.sequence
+                        , req.body.percentage
+                        , req.body.validation
+                        , req.body.innerActivation
+                        , req.body.activation
+                        , req.body.innerActivation
+                        , req.body.activation
+                        , req.body.dropOut
+                        , req.body.lossFunction
+                        , req.body.classMode
+                        , req.body.finalActivationFunction
+                    ]
             );
-            var output = "";
-            python.stdout.on('data', function(){ output += data });
-            python.on('close', function(code){
-                if (code !== 0) {  return res.send(500, code); }
-                return res.send(200, output)
+            child.stderr.on('data',function(data) {
+                console.log('Failed to start child process.');
+                res.send(500, data);
+            });
+            res.writeHead(200, { "Content-Type": "text/event-stream",
+                "Cache-control": "no-cache" });
+            //console.log('stdout'+'keras');
+            //io.sockets.on('connection', function(socket){
+            //    console.log('new user connected');
+            //    child.stdout.on('data', function(data){
+            //        output +=data.toString();
+            //        socket.emit('news', ouput);
+            //    })
+            //});
+            child.stdout.on('data', function(data){
+                console.log('stdout'+ data);
+                output += data.toString();
+                res.write('data: ' + data.toString() + "\n\n");
+            });
+            child.on('close', function(code){
+               // if (code !== 0) {  res.send(500, code); }
+                res.send(200, output)
             });
         }
 
@@ -82,73 +141,20 @@ TrainHandler.route('/')
             if (err)
                 res.send(err);
 
-            res.json(result);
-        });
-        // set timeout as high as possible
-        req.socket.setTimeout(Infinity);
-
-        // send headers for event-stream connection
-        // see spec for more information
-        res.writeHead(200, {
-            'Content-Type': 'text/event-stream',
-            'Cache-Control': 'no-cache',
-            'Connection': 'keep-alive'
-        });
-        res.write('\n');
-
-        // push this res object to our global variable
-        openConnections.push(res);
-
-        // When the request is closed, e.g. the browser window
-        // is closed. We search through the open connections
-        // array and remove this connection.
-        req.on("close", function() {
-            var toRemove;
-            for (var j =0 ; j < openConnections.length ; j++) {
-                if (openConnections[j] == res) {
-                    toRemove =j;
-                    break;
-                }
-            }
-            openConnections.splice(j,1);
-            console.log(openConnections.length);
+            res.send(result);
         });
     });
-setInterval(function() {
-    // we walk through each connection
-    openConnections.forEach(function(resp) {
-        var d = new Date();
-        resp.write('id: ' + d.getMilliseconds() + '\n');
-        resp.write('data:' + createMsg() +   '\n\n'); // Note the extra newline
-    });
 
-}, 1000);
-
-function createMsg() {
-    msg = {};
-
-    msg.hostname = os.hostname();
-    msg.type = os.type();
-    msg.platform = os.platform();
-    msg.arch = os.arch();
-    msg.release = os.release();
-    msg.uptime = os.uptime();
-    msg.loadaverage = os.loadavg();
-    msg.totalmem = os.totalmem();
-    msg.freemem = os.freemem();
-
-    return JSON.stringify(msg);
-}
-// on routes that end in /bears/:bear_id
+// on routes that end in /train/:_id
 // ----------------------------------------------------
-TrainHandler.route('/train/:record_id')
+TrainHandler.route('/:_id')
 
     // get the bear with that id
     .get(function(req, res) {
-        Map.findById(req.params.record_id, function(err, map) {
+        Train.findById(req.params._id, function(err, train) {
             if (err)
                 res.send(err);
-            res.json(map);
+            res.json(train);
         });
     });
 
