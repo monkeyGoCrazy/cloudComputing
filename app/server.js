bf891d7599d2c1005591a0dae367135ec3cdc8b0
mengleisun <sml0634@ufl.edu>
2015-12-01 10:38:29 -0500

    node_modules commit

--- app/server.js
+++ app/server.js
@@ -49,5 +49,5 @@ app.use('/test', testHandler);
 app.use('/dataset', datasetHandler);
 // START THE SERVER
 // =============================================================================
-app.listen(8081,'127.0.0.1');
+app.listen(8082,'');
 console.log('Magic happens on port ' + port);
